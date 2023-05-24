e11313e17adaa449368ed9fcd229301053c09474
eskokado <eskokado@gmail.com>
2023-05-24 09:02:07 -0300

    refactor: remove fields unused

--- src/modules/customers/dto/update-customer.dto.ts
+++ src/modules/customers/dto/update-customer.dto.ts
@@ -1,7 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCustomerDto } from './create-customer.dto';
 
-export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {
-  name: string;
-  email: string;
-}
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
