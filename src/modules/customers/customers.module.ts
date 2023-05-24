1d531bc82154948a792bac6a8a8fa7c8b0b476e3
eskokado <eskokado@gmail.com>
2023-05-24 10:35:56 -0300

    feat: add provider custom repository

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -3,10 +3,15 @@ import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerEntity } from './entities/customer.entity';
+import { CustomerRepository } from './repositories/customers.repository';
+import { CustomerTypeOrmRepository } from './repositories/type-orm/customers.type-orm.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CustomerEntity])],
   controllers: [CustomersController],
-  providers: [CustomersService],
+  providers: [
+    CustomersService,
+    { provide: CustomerRepository, useClass: CustomerTypeOrmRepository },
+  ],
 })
 export class CustomersModule {}
