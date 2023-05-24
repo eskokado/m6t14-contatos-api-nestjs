72273482fcc68ce22b29736b4e3244b66718d352
eskokado <eskokado@gmail.com>
2023-05-24 11:07:40 -0300

    feat: create entity Contact

--- src/modules/contacts/entities/contact.entity.ts
+++ src/modules/contacts/entities/contact.entity.ts
@@ -1,1 +1,37 @@
-export class Contact {}
+import { CustomerEntity } from 'src/modules/customers/entities/customer.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('contacts')
+export class ContactEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    unique: true,
+  })
+  name: string;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+
+  @ManyToOne(() => CustomerEntity, (customer) => customer.contacts)
+  customer: CustomerEntity;
+}
