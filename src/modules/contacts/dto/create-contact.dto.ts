2326476d653c2fd70e4cf31de9aab7dd01a11dcd
eskokado <eskokado@gmail.com>
2023-05-24 11:13:58 -0300

    feat: create contact dto

--- src/modules/contacts/dto/create-contact.dto.ts
+++ src/modules/contacts/dto/create-contact.dto.ts
@@ -1,1 +1,17 @@
-export class CreateContactDto {}
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class CreateContactDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+    minUppercase: 1,
+    minSymbols: 1,
+    minNumbers: 1,
+  })
+  password: string;
+}
