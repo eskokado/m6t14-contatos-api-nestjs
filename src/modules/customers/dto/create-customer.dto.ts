66cf60c5c7bb59e1f198e2f5ee93d50e3186dd30
eskokado <eskokado@gmail.com>
2023-05-24 08:44:42 -0300

    feat: add validation pipe into CreateCustomerDto

--- src/modules/customers/dto/create-customer.dto.ts
+++ src/modules/customers/dto/create-customer.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
 export class CreateCustomerDto {
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+    minUppercase: 1,
+    minSymbols: 1,
+    minNumbers: 1,
+  })
   password: string;
 }
