a993cd5b829dbef9005269ba10e75a8427b7979c
eskokado <eskokado@gmail.com>
2023-05-25 06:50:05 -0300

    refactor: remove TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './modules/customers/customers.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Customer } from './modules/customers/entities/customer.entity';
 import { DataSource } from 'typeorm';
 import { ContactsModule } from './modules/contacts/contacts.module';
-import { Contact } from './modules/contacts/entities/contact.entity';
 
 @Module({
-  imports: [
-    CustomersModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3350,
-      username: 'user',
-      password: 'pass',
-      database: 'database',
-      entities: [Customer, Contact],
-      synchronize: true,
-    }),
-    ContactsModule,
-  ],
+  imports: [CustomersModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
