9c803cfd564a5cc10b9a1c0b3db69f38ed9cd189
eskokado <eskokado@gmail.com>
2023-05-25 12:25:47 -0300

    refactor: remove AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CustomersModule } from './modules/customers/customers.module';
-import { DataSource } from 'typeorm';
 import { ContactsModule } from './modules/contacts/contacts.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [CustomersModule, ContactsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CustomersModule, ContactsModule, AuthModule],
 })
 export class AppModule {}
