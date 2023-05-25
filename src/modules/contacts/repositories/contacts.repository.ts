783b1746a750222abde70f6b83716f3a4c4c239b
eskokado <eskokado@gmail.com>
2023-05-25 07:57:21 -0300

    feat: add findByEmail into contacts repository

--- src/modules/contacts/repositories/contacts.repository.ts
+++ src/modules/contacts/repositories/contacts.repository.ts
@@ -2,12 +2,13 @@ import { CreateContactDto } from '../dto/create-contact.dto';
 import { UpdateContactDto } from '../dto/update-contact.dto';
 import { Contact } from '../entities/contact.entity';
 
-export abstract class ContactRepository {
+export abstract class ContactsRepository {
   abstract create(data: CreateContactDto): Promise<Contact> | Contact;
   abstract findAll(): Promise<Contact[]> | Contact[];
   abstract findOne(
     id: string,
   ): Promise<Contact | undefined> | Contact | undefined;
+  abstract findByEmail(email: string): Promise<Contact> | Contact;
   abstract update(
     id: string,
     data: UpdateContactDto,
