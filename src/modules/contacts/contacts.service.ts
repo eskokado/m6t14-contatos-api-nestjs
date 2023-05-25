e5be1eff45f8e5d6036a9e3d4afcd964fb2a1856
eskokado <eskokado@gmail.com>
2023-05-25 07:59:06 -0300

    refactor: fix contacts services and remove typeorm

--- src/modules/contacts/contacts.service.ts
+++ src/modules/contacts/contacts.service.ts
@@ -1,45 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Contact } from './entities/contact.entity';
-import { Repository } from 'typeorm';
+import { ContactsRepository } from './repositories/contacts.repository';
 
 @Injectable()
 export class ContactsService {
-  constructor(
-    @InjectRepository(Contact)
-    private contactRepository: Repository<Contact>,
-  ) {}
-
+  constructor(private contactsRepository: ContactsRepository) {}
   async create(createContactDto: CreateContactDto) {
-    const { name, email, password, customerId } = createContactDto;
-    return await this.contactRepository.save({
-      name,
-      email,
-      password,
-      customerId,
-    });
+    const findContact = await this.contactsRepository.findByEmail(
+      createContactDto.email,
+    );
+    if (findContact) {
+      throw new ConflictException('Contact already exists');
+    }
+    const contact = await this.contactsRepository.create(createContactDto);
+
+    return contact;
   }
 
   async findAll() {
-    return await this.contactRepository.find();
+    const contacts = await this.contactsRepository.findAll();
+    return contacts;
   }
 
   async findOne(id: string) {
-    return await this.contactRepository.findOne({
-      where: {
-        id: id,
-      },
-    });
+    const contact = await this.contactsRepository.findOne(id);
+    if (!contact) {
+      throw new NotFoundException('Contact not found !');
+    }
+    return contact;
+  }
+
+  async findByEmail(email: string) {
+    const contact = await this.contactsRepository.findOne(email);
+    return contact;
   }
 
   async update(id: string, updateContactDto: UpdateContactDto) {
-    await this.contactRepository.update(id, updateContactDto);
-    return await this.findOne(id);
+    const contact = await this.contactsRepository.update(
+      id,
+      updateContactDto,
+    );
+    return contact;
   }
 
   async remove(id: string) {
-    await this.contactRepository.delete(id);
+    await this.contactsRepository.delete(id);
+    return;
   }
 }
