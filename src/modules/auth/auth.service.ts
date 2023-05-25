3140465b2f44be012c0900d2f9d96f3d4978d227
eskokado <eskokado@gmail.com>
2023-05-25 17:23:01 -0300

    refactor: fix AuthService

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,54 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from '../../database/prisma.service';
+import { CustomersService } from '../customers/customers.service';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private customersService: CustomersService,
     private jwtService: JwtService,
-    private readonly prisma: PrismaService,
   ) {}
 
   async validateCustomer(email: string, password: string) {
-    const customer = await this.prisma.customer.findFirst({
-      where: {
-        email,
-        password,
-      },
-    });
+    const customer = await this.customersService.findByEmail(email);
+    console.log('passou aqui', customer);
     if (customer) {
-      return {
-        email: customer.email,
-        customerId: customer.id,
-        name: customer.name,
-      };
+      const passwordMatch = await compare(password, customer.password);
+      if (passwordMatch) {
+        return { email: customer.email };
+      }
     }
 
     return null;
   }
 
-  async createToken(customer: { customerId: any; name: any; email: any }) {
-    const token = this.jwtService.sign(
-      {
-        id: customer.customerId,
-        name: customer.name,
-        email: customer.email,
-      },
-      {
-        subject: String(customer.customerId),
-      },
-    );
-    return token;
-  }
-
-  async login(email: string, password: string) {
-    const customer = await this.validateCustomer(email, password);
-    if (customer) {
-      return {
-        token: await this.createToken(customer),
-        customerId: customer.customerId,
-      };
-    }
-    return null;
+  async login(email: string) {
+    const customer = await this.customersService.findByEmail(email);
+    return {
+      token: this.jwtService.sign({ email }, { subject: customer.id }),
+    };
   }
 }
