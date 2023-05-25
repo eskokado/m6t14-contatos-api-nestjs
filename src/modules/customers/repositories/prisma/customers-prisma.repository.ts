a38fbe4846cac7e85dd2400a20efe2ac7dc71e65
eskokado <eskokado@gmail.com>
2023-05-25 06:43:59 -0300

    feat: create customers prisma repository

---
+++ src/modules/customers/repositories/prisma/customers-prisma.repository.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { CustomersRepository } from '../customers.repository';
+import { CreateCustomerDto } from '../../dto/create-customer.dto';
+import { UpdateCustomerDto } from '../../dto/update-customer.dto';
+import { Customer } from '../../entities/customer.entity';
+import { PrismaService } from 'src/database/prisma.service';
+import { plainToInstance } from 'class-transformer';
+
+@Injectable()
+export class CustomersPrismaRepository implements CustomersRepository {
+  constructor(private prisma: PrismaService) {}
+  async create(data: CreateCustomerDto): Promise<Customer> {
+    const customer = new Customer();
+    Object.assign(customer, {
+      ...data,
+    });
+
+    const newCustomer = await this.prisma.customer.create({
+      data: { ...customer },
+    });
+
+    return plainToInstance(Customer, newCustomer);
+  }
+  async findAll(): Promise<Customer[]> {
+    const customers = await this.prisma.customer.findMany();
+    return plainToInstance(Customer, customers);
+  }
+  async findOne(id: string): Promise<Customer> {
+    const customer = await this.prisma.customer.findUnique({
+      where: { id },
+    });
+    return plainToInstance(Customer, customer);
+  }
+  async findByEmail(email: string): Promise<Customer> {
+    const customer = await this.prisma.customer.findUnique({
+      where: { email },
+    });
+    return plainToInstance(Customer, customer);
+  }
+  async update(id: string, data: UpdateCustomerDto): Promise<Customer> {
+    const customer = await this.prisma.customer.update({
+      where: { id },
+      data: { ...data },
+    });
+
+    return plainToInstance(Customer, customer);
+  }
+  async delete(id: string): Promise<void> {
+    await this.prisma.customer.delete({
+      where: { id },
+    });
+  }
+}
