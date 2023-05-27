94ef597e50ccad7565c51521b2d793e259ee247c
eskokado <eskokado@gmail.com>
2023-05-27 14:14:01 -0300

    feat: register PrismaService and PhonesCustomerRepository into PhonesCustomersModule

--- src/modules/phones-customers/phones-customers.module.ts
+++ src/modules/phones-customers/phones-customers.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PhonesCustomersService } from './phones-customers.service';
 import { PhonesCustomersController } from './phones-customers.controller';
+import { PrismaService } from '../../database/prisma.service';
+import { PhonesCustomersRepository } from './repositories/phones-customers.repository';
+import { PhonesCustomersPrismaRepository } from './repositories/prisma/phones-customer.prisma.repository';
 
 @Module({
   controllers: [PhonesCustomersController],
-  providers: [PhonesCustomersService]
+  providers: [
+    PhonesCustomersService,
+    PrismaService,
+    {
+      provide: PhonesCustomersRepository,
+      useClass: PhonesCustomersPrismaRepository,
+    },
+  ],
 })
 export class PhonesCustomersModule {}
