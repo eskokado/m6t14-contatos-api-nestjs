465e2c8f9c372ea8d63cd667a9bf1931484e0c10
eskokado <eskokado@gmail.com>
2023-05-27 14:07:47 -0300

    feat: add constructor to id generated uuid

--- src/modules/phones-customers/entities/phones-customer.entity.ts
+++ src/modules/phones-customers/entities/phones-customer.entity.ts
@@ -1,7 +1,12 @@
+import { randomUUID } from 'crypto';
+
 export class PhonesCustomer {
   readonly id: string;
 
   name: string;
 
   number: string;
+  constructor() {
+    this.id = randomUUID();
+  }
 }
