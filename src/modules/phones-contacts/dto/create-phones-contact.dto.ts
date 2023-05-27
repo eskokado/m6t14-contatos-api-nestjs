ecb766d6f51435ea3c0a919b0604d7f596827583
eskokado <eskokado@gmail.com>
2023-05-27 15:06:26 -0300

    feat: create dto create-phones-contact.dto.ts

--- src/modules/phones-contacts/dto/create-phones-contact.dto.ts
+++ src/modules/phones-contacts/dto/create-phones-contact.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePhonesContactDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreatePhonesContactDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  number: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  contactId: string;
+}
